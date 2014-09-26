9d74a659153f4fc3bcfda3d170fe34e9424095fb
txgruppi <txgruppi@gmail.com>
2014-09-26 10:55:57 +0100

    Get coveralls token from env

--- coverage.sh
+++ coverage.sh
@@ -12,7 +12,7 @@ test -f db.coverage.out && tail -n+2 db.coverage.out >> coverage.out
 test -f urlshortener.coverage.out && tail -n+2 urlshortener.coverage.out >> coverage.out
 
 if [ "$(cat coverage.out | wc -l)" -gt 1 ]; then
-  $(go env GOPATH | awk 'BEGIN{FS=":"} {print $1}')/bin/goveralls -coverprofile=coverage.out -service=codeship -repotoken AA5OvxIquBCzVvXj0NNge0LpAakBiLuiC
+  $(go env GOPATH | awk 'BEGIN{FS=":"} {print $1}')/bin/goveralls -coverprofile=coverage.out -service=codeship -repotoken "$COVERALLS_TOKEN"
 fi
 
 rm *.out
